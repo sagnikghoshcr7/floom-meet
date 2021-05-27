f1143afcff587dcf1c121848a6af7a7b2c2d73a7
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-05-27 19:33:17 +0530

    add signup_screen

--- lib/screens/authentication/loginScreen.dart
+++ lib/screens/authentication/loginScreen.dart
@@ -1,4 +1,6 @@
+import 'package:floom_meet/variables.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gradient_colors/flutter_gradient_colors.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -9,7 +11,98 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.grey[250],
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: GradientColors.blue),
+            ),
+            child: Center(
+              child: Image.asset(
+                'assets/logo.png',
+                height: 100,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.6,
+              margin: EdgeInsets.only(left: 30, right: 30),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 50),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.7,
+                    child: TextField(
+                      style: mystyle(18, Colors.black),
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        prefixIcon: Icon(Icons.email),
+                        hintStyle: mystyle(20, Colors.grey, FontWeight.w700),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.7,
+                    child: TextField(
+                      style: mystyle(18, Colors.black),
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        prefixIcon: Icon(Icons.lock),
+                        hintStyle: mystyle(20, Colors.grey, FontWeight.w700),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: GradientColors.beautifulGreen,
+                        ),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "SIGN IN",
+                          style: mystyle(25, Colors.white),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
