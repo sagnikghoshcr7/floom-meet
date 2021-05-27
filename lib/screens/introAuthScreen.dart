434301e6e31e46633cdc34f860b6db00d244b82a
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-05-27 17:12:26 +0530

    add initial screens

---
+++ lib/screens/introAuthScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IntroAuthScreen extends StatefulWidget {
+  @override
+  _IntroAuthScreenState createState() => _IntroAuthScreenState();
+}
+
+class _IntroAuthScreenState extends State<IntroAuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
