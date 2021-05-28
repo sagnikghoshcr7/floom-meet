1b42dececad296b5d46253ed1b16fa21621a54bf
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-05-28 22:06:35 +0530

    create videoConferenceScreen & profileScreen

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,4 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:floom_meet/screens/profileScreen.dart';
+import 'package:floom_meet/screens/videoConferenceScreen.dart';
+import 'package:floom_meet/variables.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,14 +10,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int page = 0;
+  List pageoptions = [
+    VideoConferenceScreen(),
+    ProfileScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: InkWell(
-        onTap: () {
-          FirebaseAuth.instance.signOut();
+      backgroundColor: Colors.grey[250],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.blue,
+        selectedLabelStyle: mystyle(17, Colors.blue),
+        unselectedItemColor: Colors.black,
+        unselectedLabelStyle: mystyle(17, Colors.black),
+        currentIndex: page,
+        onTap: (index) {
+          setState(() {
+            page = index;
+          });
         },
+        items: [
+          BottomNavigationBarItem(
+            label: "Video Call",
+            icon: Icon(Icons.video_call, size: 32),
+          ),
+          BottomNavigationBarItem(
+            label: "Profile",
+            icon: Icon(Icons.video_call, size: 32),
+          ),
+        ],
       ),
+      body: pageoptions[page],
     );
   }
 }
---
+++ lib/screens/videoConferenceScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoConferenceScreen extends StatefulWidget {
+  @override
+  _VideoConferenceScreenState createState() => _VideoConferenceScreenState();
+}
+
+class _VideoConferenceScreenState extends State<VideoConferenceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
